4df77700ba01aa99fe7285f88a830d90d1d1f902
Rehan Ali maan <130174371+RehanAliMaan@users.noreply.github.com>
2023-10-27 15:46:10 +0500

    Add files via upload

---
+++ c12.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+	int dis,spe,tim;
+	cout<<"enter the distance"<<endl;
+	cin>>dis;
+	cout<<"enter the speed"<<endl;
+	cin>>spe;
+	tim=dis/spe;
+	cout<<"the time is"<<tim<<endl;
+	
+}
